cb375018efdbc0a6c40e7b9f669a25900a0c5a98
Mrunmayi Dharmadhikari <mrunmayi016@gmail.com>
2021-05-31 08:48:57 -0700

    Create and display list of ingredients

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,52 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { getRecipes } from "./service/Recipes";
 
 function App() {
-  const ingredients = ["apples", "flour", "sugar"];
+  const [ingredients, setIngredients] = useState([]);
 
-  const [recipes, setRecipes] = useState();
+  //const [recipes, setRecipes] = useState();
 
-  useEffect(function() {
-    getRecipes(ingredients).then((recipes) => {setRecipes(recipes as any)} )
+  useEffect(function () {
+    // getRecipes(ingredients).then((recipes) => {setRecipes(recipes as any)} )
+    console.log(ingredients);
   });
 
-  return <div> {JSON.stringify(recipes)} </div>;
+  const listMap = (element: String) => {
+    return <li>{element}</li>;
+  };
+
+  const inputRef = useRef();
+
+  const keyPressed = (event : any) => {
+    if(event.key === "Enter") {
+      addItem();
+    }
+  }
+
+  const addItem = () => {
+    const item = (inputRef?.current as any).value;
+    (inputRef?.current as any).value = "";
+    setIngredients([...ingredients, item] as any);
+  };
+
+  return (
+    <div>
+      <h1>Recipe Recommendations</h1>
+      <div>
+        <input type="text" placeholder="enter the ingredients" ref={inputRef as any} onKeyPress = {keyPressed} />
+
+        <button onClick={addItem}>Add Ingredient(s)</button>
+
+        <button>Search</button>
+      </div>
+      <div>
+        <ul>{ingredients.map(listMap)}</ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
+
+//{JSON.stringify(recipes)}
