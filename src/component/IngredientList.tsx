b6c75861b34aa22294803af2d237d0207899120a
Mrunmayi Dharmadhikari <mrunmayi016@gmail.com>
2021-06-03 11:53:46 -0700

    Improve user experience and interface design

--- src/component/IngredientList.tsx
+++ src/component/IngredientList.tsx
@@ -2,7 +2,6 @@ import { useIngredientsContext } from "context/RecipeSearchContext";
 import React from "react";
 import { useRef } from "react";
 
-
 function IngredientList(): React.ReactElement {
   const inputRef = useRef();
   const [ingredients, setIngredients] = useIngredientsContext();
@@ -15,28 +14,44 @@ function IngredientList(): React.ReactElement {
 
   const addItem = () => {
     const item = (inputRef?.current as any).value;
+    if (item === "") return;
+    item.trim();
     (inputRef?.current as any).value = "";
     setIngredients([...ingredients, item] as any);
   };
 
   const listMap = (element: String) => {
-    return <li>{element}</li>;
+    return (
+      <li className="ml-2 my-1">
+        <div className="flex bg-white px-4 py-2 rounded-full shadow-lg text-gray-800 font-light">
+          <p className="">{element}</p>
+          <p className="ml-2 cursor-pointer">x</p>
+        </div>
+      </li>
+    );
   };
 
   return (
     <div>
       <div>
         <input
+          className="px-6 py-4 rounded-full focus:outline-none shadow-md"
           type="text"
-          placeholder="enter the ingredients"
+          placeholder="Enter the ingredients"
           ref={inputRef as any}
           onKeyPress={keyPressed}
         />
 
-        <button onClick={addItem}>Add Ingredient(s)</button>
+        <button
+          className="ml-10 border-blue-400 border-2 px-6 py-4 rounded-full text-blue-400 hover:bg-blue-100 focus:outline-none"
+          onClick={addItem}
+        >
+          Add Ingredient
+        </button>
       </div>
-      <div>
-        <ul>{ingredients.map(listMap)}</ul>
+      <div className="mt-6">
+        <h3>{ingredients.length ? "Your Pantry Ingredients:" : ""}</h3>
+        <ul className="my-2 flex flex-wrap">{ingredients.map(listMap)}</ul>
       </div>
     </div>
   );
--- src/module/RecipeSearch.tsx
+++ src/module/RecipeSearch.tsx
@@ -18,11 +18,29 @@ function RecipeSearch(): React.ReactElement {
   };
 
   return (
-    <div>
-      <h1>Recipe Recommendations</h1>
-      <IngredientList />
-      <button onClick={searchHandler}>Search</button>
-      <RecipesList />
+    <div className="bg-gray-50 h-screen flex justify-center">
+      <div className="w-3/5">
+        <div className="mt-16" >
+          <h1 className="font-semibold text-5xl" >Cook with what you have</h1>
+          <p className="mt-8 text-xl font-light leading-relaxed" >
+            Binge-eating or Stress-eating? <br />
+            We've got you covered! <br />
+            Craving a midnight snack or a healthy meal? <br />
+            We've got you covered!
+          </p>
+        </div>
+
+        <div className="mt-10">
+          <IngredientList />
+          
+          <button className="mt-8 bg-blue-400 px-6 py-4 text-white rounded-full"  
+          onClick={searchHandler}>Search Recipes</button>
+        </div>
+
+        <div>
+          <RecipesList />
+        </div>
+      </div>
     </div>
   );
 }
